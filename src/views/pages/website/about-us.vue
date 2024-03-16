1ba805b17dcfe4211a02b986b7313ebf8f5bc7af
Sameh Bilal <sameh.bilal@outlook.com>
2024-03-16 16:57:01 +0200

    updates

--- src/views/pages/website/about-us.vue
+++ src/views/pages/website/about-us.vue
@@ -9,13 +9,13 @@ export default {
     components: { Layout, PageHeader },
     data() {
         return {
-            title: "About Us",
+            title: "How Are We",
             items: [
                 {
                     text: "Homepage"
                 },
                 {
-                    text: "Services",
+                    text: "About Us",
                     active: true
                 },
             ],
@@ -28,6 +28,253 @@ export default {
 
 <template>
     <Layout>
-        <PageHeader :title="title" :items="items" :desc="desc" />
+        <!-- <PageHeader :title="title" :items="items" :desc="desc" /> -->
+        <div class="page-about-group-19">
+            <div class="page-about-group-20">
+                <div class="page-about-overlap-group-4">
+                    <div class="page-about-rectangle-7"></div>
+                    <div class="page-about-text-wrapper-8">How Are We</div>
+                </div>
+            </div>
+            <div class="page-about-group-21">
+                <div class="page-about-homepage">Homepage</div>
+                <div class="page-about-overlap-4">
+                    <div class="page-about-text-wrapper-9">About Us</div>
+                    <img class="page-about-vector-2" src="https://c.animaapp.com/DtRBl1fX/img/vector-59@2x.png" />
+                </div>
+                <div class="page-about-rectangle-8"></div>
+                <img class="page-about-vector-3" src="https://c.animaapp.com/DtRBl1fX/img/vector-58-1@2x.png" />
+            </div>
+        </div>
+        <div class="page-about-group-17">
+            <div class="page-about-img-3">
+                <img class="page-about-element-8" src="https://c.animaapp.com/DtRBl1fX/img/1-2@2x.png" />
+                <img class="page-about-element-9" src="https://c.animaapp.com/DtRBl1fX/img/2-2@2x.png" />
+                <img class="page-about-element-10" src="https://c.animaapp.com/DtRBl1fX/img/3@2x.png" />
+                <div class="page-about-play">
+                    <div class="page-about-watch-the-promo">WATCH THE PROMO</div>
+                    <div class="page-about-icon"><img class="page-about-fi-play" src="https://c.animaapp.com/DtRBl1fX/img/fi-play-1@2x.png" />
+                    </div>
+                </div>
+            </div>
+            <div class="page-about-group-18">
+                <div class="page-about-flexcontainer-2">
+                    <p class="page-about-text">
+                        <span class="page-about-text-wrapper">Arabia Talents, formed in 2018, is a leading influencer marketing
+                            agency in the
+                            Middle East,
+                            providing comprehensive marketing solutions for businesses looking to connect with their
+                            target
+                            audience in the Arab world.&nbsp;&nbsp;<br /></span>
+                    </p>
+                    <p class="page-about-text">
+                        <span class="page-about-text-wrapper">We specialize in finding the right influencers, content creators, and
+                            on-air
+                            talents that align with
+                            your brand, and help you to organize events, manage brand promotions, and execute digital
+                            advertising
+                            campaigns.&nbsp;&nbsp;<br /></span>
+                    </p>
+                    <p class="page-about-text">
+                        <span class="page-about-text-wrapper">With our expertise and deep understanding of the esports and digital
+                            media
+                            landscape in the Middle
+                            East, we are able to deliver high ROI for our clients.&nbsp;&nbsp;<br /></span>
+                    </p>
+                    <p class="page-about-text">
+                        <span class="page-about-text-wrapper">Our dedicated team of career counselors ensures that our talents are
+                            equipped
+                            with the skills and
+                            resources to build successful, long-term careers in the ever-evolving digital
+                            space.&nbsp;&nbsp;<br /></span>
+                    </p>
+                    <p class="page-about-text">
+                        <span class="page-about-text-wrapper">We are committed to bringing brands and talents together in the most
+                            effective
+                            and efficient way
+                            possible, to help drive business growth and success. <br /></span>
+                    </p>
+                    <p class="page-about-text"><span class="page-about-text-wrapper"></span></p>
+                </div>
+                <div class="page-about-text-wrapper-7">Our Story:</div>
+            </div>
+        </div>
+        <img class="page-about-dots-2" src="https://c.animaapp.com/DtRBl1fX/img/dots-1@2x.png" />
+        <div class="page-about-overlap">
+            <img class="page-about-vector" src="https://c.animaapp.com/DtRBl1fX/img/vector-3.png" />
+            <div class="page-about-BG"></div>
+            <div class="page-about-group">
+                <div class="page-about-flexcontainer">
+                    <p class="page-about-text">
+                        <span class="page-about-text-wrapper">Arabia Talents Agency is a digital marketing and communication agency
+                            which
+                            serves both brands and
+                            talents. <br /></span>
+                    </p>
+                    <p class="page-about-text">
+                        <span class="page-about-text-wrapper">By developing unique marketing and communication campaigns for
+                            brands, we aim
+                            to enhance their
+                            presence and enable them to express themselves on leading social and digital media
+                            platforms,
+                            especially in Twitch, Instagram and YouTube.<br /></span>
+                    </p>
+                    <p class="page-about-text"><span class="page-about-text-wrapper"></span></p>
+                </div>
+                <p class="page-about-title">The Leading Mena Agency For Influencer Marketing.</p>
+                <div class="page-about-div">
+                    <div class="page-about-element-wrapper">
+                        <div class="page-about-element">
+                            <div class="page-about-ellipse"></div>
+                            <div class="page-about-title-2">Influencer Marketing</div>
+                        </div>
+                    </div>
+                    <div class="page-about-div-wrapper">
+                        <div class="page-about-element-2">
+                            <div class="page-about-ellipse"></div>
+                            <div class="page-about-title-2">Talent Management</div>
+                        </div>
+                    </div>
+                    <div class="page-about-group-2">
+                        <div class="page-about-element-3">
+                            <div class="page-about-ellipse"></div>
+                            <div class="page-about-title-2">Branded Content</div>
+                        </div>
+                    </div>
+                    <div class="page-about-group-3">
+                        <div class="page-about-element-4">
+                            <div class="page-about-ellipse"></div>
+                            <div class="page-about-title-2">Offline Operations</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="page-about-title-3">And Much More...</div>
+            </div>
+            <div class="page-about-img">
+                <img class="page-about-element-5" src="https://c.animaapp.com/DtRBl1fX/img/1-3@2x.png" />
+                <img class="page-about-element-6" src="https://c.animaapp.com/DtRBl1fX/img/2-3@2x.png" />
+                <img class="page-about-element-7" src="https://c.animaapp.com/DtRBl1fX/img/3-1@2x.png" />
+                <div class="page-about-BG-wrapper">
+                    <div class="page-about-img-wrapper"><img class="page-about-BG-2" src="https://c.animaapp.com/DtRBl1fX/img/bg@2x.png" />
+                    </div>
+                </div>
+            </div>
+            <img class="page-about-dots" src="https://c.animaapp.com/DtRBl1fX/img/dots@2x.png" />
+        </div>
+        <div class="page-about-overlap-group">
+            <div class="page-about-group-4"></div>
+            <div class="page-about-group-5">
+                <div class="page-about-group-6">
+                    <div class="page-about-overlap-group-2">
+                        <div class="page-about-rectangle"></div>
+                        <p class="page-about-we-generated-more">We Generated 3x More Engagement In 2022</p>
+                    </div>
+                    <p class="page-about-through-strategy">
+                        Through Strategy, Planning, And Marketing We Built Strong Relationships And Cooperation Between
+                        Brands
+                        And Talents Which Led To Increasing Brand Awareness, And Social Media Engagement For Both Sides.
+                    </p>
+                </div>
+                <div class="page-about-group-7">
+                    <div class="page-about-overlap-2">
+                        <img class="page-about-dot" src="https://c.animaapp.com/DtRBl1fX/img/dot@2x.png" />
+                        <div class="page-about-ellipse-2"></div>
+                        <div class="page-about-ellipse-3"></div>
+                        <div class="page-about-ellipse-4"></div>
+                        <div class="page-about-ellipse-5"></div>
+                        <div class="page-about-ellipse-6"></div>
+                        <div class="page-about-ellipse-7"></div>
+                        <div class="page-about-ellipse-8"></div>
+                        <div class="page-about-ellipse-9"></div>
+                        <div class="page-about-ellipse-10"></div>
+                        <div class="page-about-ellipse-11"></div>
+                        <div class="page-about-ellipse-12"></div>
+                        <div class="page-about-ellipse-13"></div>
+                        <div class="page-about-ellipse-14"></div>
+                        <div class="page-about-ellipse-15"></div>
+                        <div class="page-about-ellipse-16"></div>
+                        <div class="page-about-ellipse-17"></div>
+                        <div class="page-about-ellipse-18"></div>
+                        <div class="page-about-ellipse-19"></div>
+                        <div class="page-about-ellipse-20"></div>
+                        <div class="page-about-ellipse-21"></div>
+                        <div class="page-about-ellipse-22"></div>
+                        <div class="page-about-ellipse-23"></div>
+                        <div class="page-about-ellipse-24"></div>
+                        <div class="page-about-ellipse-25"></div>
+                        <div class="page-about-ellipse-26"></div>
+                        <div class="page-about-ellipse-27"></div>
+                        <div class="page-about-ellipse-28"></div>
+                        <div class="page-about-ellipse-29"></div>
+                        <div class="page-about-ellipse-30"></div>
+                        <div class="page-about-ellipse-31"></div>
+                        <div class="page-about-ellipse-32"></div>
+                        <div class="page-about-ellipse-33"></div>
+                        <div class="page-about-rectangle-2"></div>
+                        <div class="page-about-overlap-wrapper">
+                            <div class="page-about-overlap-3">
+                                <div class="page-about-overlap-group-wrapper">
+                                    <div class="page-about-overlap-group-3">
+                                        <div class="page-about-rectangle-3"></div>
+                                        <div class="page-about-rectangle-4"></div>
+                                        <div class="page-about-group-8">
+                                            <div class="page-about-text-wrapper-2">+128</div>
+                                            <div class="page-about-text-wrapper-3">Projects Done</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <img class="page-about-group-9" src="https://c.animaapp.com/DtRBl1fX/img/group-13411@2x.png" />
+                            </div>
+                        </div>
+                        <div class="page-about-group-10">
+                            <div class="page-about-overlap-3">
+                                <div class="page-about-overlap-group-wrapper">
+                                    <div class="page-about-overlap-group-3">
+                                        <div class="page-about-rectangle-3"></div>
+                                        <div class="page-about-rectangle-5"></div>
+                                        <div class="page-about-group-11">
+                                            <div class="page-about-text-wrapper-4">+10</div>
+                                            <div class="page-about-text-wrapper-3">Exclusive Deals</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <img class="page-about-group-12" src="https://c.animaapp.com/DtRBl1fX/img/group-13555@2x.png" />
+                            </div>
+                        </div>
+                        <div class="page-about-group-13">
+                            <div class="page-about-overlap-group-3">
+                                <div class="page-about-rectangle-3"></div>
+                                <div class="page-about-rectangle-5"></div>
+                                <div class="page-about-group-14">
+                                    <div class="page-about-text-wrapper-5">+50</div>
+                                    <div class="page-about-text-wrapper-3">Clients Onboard</div>
+                                </div>
+                                <img class="page-about-img-2" src="https://c.animaapp.com/DtRBl1fX/img/group-13708@2x.png" />
+                            </div>
+                        </div>
+                        <div class="page-about-group-15">
+                            <div class="page-about-overlap-group-3">
+                                <div class="page-about-rectangle-3"></div>
+                                <div class="page-about-rectangle-6"></div>
+                                <div class="page-about-group-16">
+                                    <div class="page-about-text-wrapper-6">1 Billion</div>
+                                    <div class="page-about-views-in">Views In 2021</div>
+                                </div>
+                                <img class="page-about-img-2"
+                                    src="https://c.animaapp.com/DtRBl1fX/img/vuesax-outline-eye@2x.png" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="page-about-ellipse-34"></div>
+                    <div class="page-about-ellipse-35"></div>
+                    <div class="page-about-ellipse-36"></div>
+                    <div class="page-about-ellipse-37"></div>
+                    <div class="page-about-ellipse-38"></div>
+                    <div class="page-about-ellipse-39"></div>
+                    <div class="page-about-ellipse-40"></div>
+                    <div class="page-about-ellipse-41"></div>
+                </div>
+            </div>
+        </div>
     </Layout>
 </template>
